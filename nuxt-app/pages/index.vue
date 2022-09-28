0b3f4a67393cda175eb79b730a2a8e29a5435b23
aipa <k.tamashiro@churadata.okinawa>
2022-09-28 21:23:16 +0900

    [update] 画像を渡して利用できるようにした.

--- nuxt-app/pages/index.vue
+++ nuxt-app/pages/index.vue
@@ -1,7 +1,34 @@
 <template>
     <h1>Index page</h1>
     <ClientOnly>
-        <FormKit type="text" label="Number" validation="required|number|between:20,50" validation-visibility="live"
-            help="Enter a number between 20 and 50." />
+        <FormKit
+            type="form"
+            @submit="submit"
+            submit-label="登録する"
+            incomplete-message="入力内容に不備があるよ">
+            <FormKitSchema :schema="schema"/>
+        </FormKit>
     </ClientOnly>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { FormKitSchemaNode } from '@formkit/core'
+
+const nameFormSchema: FormKitSchemaNode = {
+  $cmp: "FormKit",
+  props: {
+    type: "file",
+    label: "画像",
+    name: "nameForm",
+    accept: ".gif,.png,jpg",
+    help: "画像を選択してください。",
+    multiple: true,
+  }
+}
+const schema = [nameFormSchema]
+const submit = async (e: {nameForm: string}) => {
+  // 好きな処理を実装してね （フォームのname属性をキー、入力値がvalueのオブジェクトが手に入る）
+  console.log(e)
+};
+</script>
