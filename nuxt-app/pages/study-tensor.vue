89b473a798f6da313471dbb4fe58bb7b29c81ada
aipa <k.tamashiro@churadata.okinawa>
2022-11-13 23:33:03 +0900

    [update] ssdで遊ぶ.

---
+++ nuxt-app/pages/study-tensor.vue
@@ -0,0 +1,46 @@
+<template>
+    <h1>
+        テスト
+    </h1>
+    <div>
+        <a href="#">テスト</a>
+        <video id="video"></video>
+        <canvas id="canvas"></canvas>
+    </div>
+</template>
+
+<script setup lang="ts">
+import * as tf from '@tensorflow/tfjs'
+import * as poseDetection from '@tensorflow-models/pose-detection'
+import '@tensorflow/tfjs-backend-webgl';
+import { onMounted } from 'vue';
+
+const model = poseDetection.SupportedModels.BlazePose
+const detectorConfig = {
+  runtime: 'tfjs',
+  modelType: 'full'
+}
+
+onMounted(async() => {
+    const detector = await poseDetection.createDetector(model, detectorConfig)
+    const video = document.getElementById('video')
+    navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: false,
+    }).then(async(stream) => {
+        video.srcObject = stream;
+        video.play()
+        const canvas = document.getElementById('canvas')
+        const ctx = canvas.getContext('2d')
+        canvas.setAttribute("width", 100)
+        canvas.setAttribute("height", 100)
+        ctx.drawImage(video, 0, 0, 100, 100)
+        const poses = await detector.estimatePoses(canvas)
+        console.log(poses)
+    }).catch(e => {
+        console.log(e)
+    })
+})
+
+
+</script>
---
+++ nuxt-app/pages/study.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>テスト</h1>
+    <img id="mystery" />
+    <div style="position: relative;">
+      <video ref="mystery" width="100%" autoplay></video>
+      <canvas ref="detection" style="position: absolute; left: 0;"></canvas>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import { doStuff } from '../lib/ssd/detect'
+
+const mystery = ref<HTMLImageElement>(null)
+const detection = ref<HTMLImageElement>(null)
+onMounted(() => {
+  doStuff(mystery, detection)
+})
+</script>
